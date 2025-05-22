3de9568f44419f3adf1a7e8d8d614dc279680967
baritaha <135865393+baritaha@users.noreply.github.com>
2025-05-22 18:37:39 +0300

    Task#1

---
+++ script.js
@@ -0,0 +1,20 @@
+const name=document.querySelector(".name");
+
+function changeBlue()
+{
+    name.style.color="blue";
+    name.style.backgroundColor="aqua";
+    console.log("hello world from blue");
+}
+function changeRed()
+{
+    name.style.color="red";
+    name.style.backgroundColor="pink";
+    console.log("hello world from red");
+}
+function changeGreen()
+{
+    name.style.color="green";
+    name.style.backgroundColor="lightgreen";
+    console.log("hello world from green");
+}
