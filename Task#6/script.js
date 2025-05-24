43e0a0f2d3406f267a2d897e0232d8c3daa16711
baritaha <135865393+baritaha@users.noreply.github.com>
2025-05-24 19:11:18 +0300

    update task#6

--- Task#6/script.js
+++ Task#6/script.js
@@ -1,25 +1,18 @@
 const buttonAPI = document.getElementById('apiButton');
 document.getElementById('apiButton').addEventListener('click', function () {
-    const button = this;
-    button.disabled = true;
-    button.textContent = 'Loading...';
+
     fetch('https://api.ipify.org/')
         .then(response => {
-              buttonAPI.style.color = 'white';
+            buttonAPI.style.color = 'white';
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.text();
         })
         .then(ip => {
-            setTimeout(() => {
-                button.textContent = ip;
-                button.disabled = false;
-            }, 1000);
+            console.log('Your IP address is:', ip);
         })
         .catch(error => {
             console.error('Error fetching IP:', error);
-            button.textContent = 'Error getting IP';
-            button.disabled = false;
         });
 });
