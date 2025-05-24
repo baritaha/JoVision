6adc3759be13d7b4f5fc1f0db41b7bdfd84abc9f
baritaha <135865393+baritaha@users.noreply.github.com>
2025-05-24 18:48:58 +0300

    start add from task#6

--- Task#1/script.js
+++
@@ -1,9 +0,0 @@
-const name=document.querySelector(".name");
-
-function ExecuteFn(color,backGround,text)
-{
-    name.style.color=color;
-    name.style.backgroundColor=backGround;
-
-    console.log(`${text} from ${color}`)
-}
